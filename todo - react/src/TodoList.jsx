353837d2ce6a50efede5ad6f473b9aaef3e94001
HARSH JOSHI <115775581+harshjoshi1312@users.noreply.github.com>
2024-01-03 20:44:39 +0530

    Add files via upload

---
+++ todo - react/src/TodoList.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+
+
+export default function TodoList() {
+
+    let [todos,setTodos] = useState(["wow"]);
+    let [newTodo, setNewTodo] = useState("");
+
+    let addNewTask = () => {
+        setTodos([...todos,newTodo]);
+        setNewTodo("");
+    };
+
+    let updateTodoValue = (event)=>{
+        setNewTodo(event.target.value);
+    };
+
+    return (
+        <div>
+        <input 
+        placeholder="add a task" 
+        value={newTodo} 
+        onChange={updateTodoValue}>
+        </input>
+        <button onClick={addNewTask} >add task</button>
+        <br /><br /><br /><br /><hr />
+        <h4> TAsk Todo </h4>
+        <ul>
+            {todos.map((todo) => (
+                <li>{todo}</li>
+            ))}
+               
+     </ul>
+        </div>
+    );
+}
